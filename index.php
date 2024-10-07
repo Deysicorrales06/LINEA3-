a623fe561d25899ddb4579de1232fba6e2b13323
Deysicorrales06 <76816143@idexperujapon.edu.pe>
2024-10-06 19:42:51 -0500

    LINEA3

---
+++ index.php
@@ -0,0 +1,38 @@
+<body class="px-3">
+    <?php include 'Partes/header.php'; ?>
+    <div class="container mt-4">
+        <h1> SOLUCIONES CREATIVAS </h1>
+        <p> Sin importar cual sea tu negocio, nuestro equipo puede hacerlo desde cero.</p>
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="car-body">
+                    <img class="w-100" src="./resources/img/idea_01.PNG" alt="">
+                    <h5 class=h3>
+                        <i class="bi bi-pen"></i>DISEÑO
+                    </h5>
+                    <p class="pt-0">Creamos diseño segun tus necesidades.</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div clas="card boder-0">
+                    <img class="w-100" src="./resources/img/idea_02.PNG" alt="">
+                    <h5 class="h5 mt-2">
+                        <i class="bi bi-flower1"></i>DESARROLLO
+                    </h5>
+                    <p>Nuestros Programadores te garantizaran la perfeccion.</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div clas="card boder-0">
+                    <img class="w-100" src="./resources/img/idea_03.PNG" alt="">
+                    <h5 class="h5 mt-2">
+                        <i class="bi bi-plus-square-fill"></i>EDICION
+                    </h5>
+                    <p>Que el producto final llegue a tus clientes .</p>
+                </div>
+            </div>
+        </div>
+        <?php include 'Partes/footer/php'; ?>
+</body>
+
+</html>
